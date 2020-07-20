48a053b474741ecae4577b684512e3bc00c71482
rfrielinck <68222391+rfrielinck@users.noreply.github.com>
2020-07-20 16:16:00 +0200

    Buy and supply button, maar de css werkt niet

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
 import "./main.css";
+import "./Buyandsupplybuttons.css";
+import "./navbar.css";
 
 function Main() {
   return (
     <div>
       <div className="header" />
+      <div class="row">
+          
+          <Link to="/Buyer">
+          <li>   
+          <div class="left">
+          <p>Buyers</p>
+          <div class="secondary-text">
+              <p>Send Quote request</p>
+              </div>
+          </div>
+          </li>
+          </Link>
+          
+          <Link to="/Supplier">
+          <li>   
+          <div class="right">
+          <p>Suppliers</p>
+          <div class="secondary-text">
+              <p>Send Quote request</p>
+              </div>
+          </div>
+          </li>
+          </Link>
+        </div>
+      
       <div className="content" />
+      
       <div className="footer">
       <div className="footertext">
             <p>
