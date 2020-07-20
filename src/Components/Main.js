73666e8e4eb70aa3c1d975e47385625ecc90a71b
rfrielinck <68222391+rfrielinck@users.noreply.github.com>
2020-07-20 16:43:32 +0200

    About Us added on main-page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,7 +8,15 @@ import "./navbar.css";
 function Main() {
   return (
     <div>
-      <div className="header" />
+<div class="headertext">
+<p>About Metal Allies</p></div>
+  <div class="text"><p>
+  Metal Allies is a global platform connecting international buyers with suppliers in Europe. A starting online sourcing platform for OEM, custom parts and components, helping companies in procurement and meeting output quotas.
+  <br></br>
+  <br></br>
+  We have customers in 0 countries, increasing production efficiency and reducing manufacturing costs. We have partnerships with zero's of industry supply chain service providers, both upstream and downstream.
+  </p></div>
+      
       <div class="row">
           
           <Link to="/Buyer">
@@ -27,7 +35,7 @@ function Main() {
           <div class="right">
           <p>Suppliers</p>
           <div class="secondary-text">
-              <p>Send Quote request</p>
+              <p>Receive Quote request</p>
               </div>
           </div>
           </li>
